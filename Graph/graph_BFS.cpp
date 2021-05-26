b12d8fddf9209ac44ef5c1b37acaa114269c6670
S3-20181650 <39684697+S3-20181650@users.noreply.github.com>
2021-05-26 19:26:34 +0900

    commit

--- Graph/graph_BFS.cpp
+++ Graph/graph_BFS.cpp
@@ -1,6 +1,6 @@
 // program : lab10_육종호.cpp
-// Name : 육종호
-// ID : 20181650
+// 이름 : 육종호
+// 학번 : 20181650
 
 #include <iostream>
 #include <cstring>
@@ -39,7 +39,7 @@ public:
     void enqueue(int);
     int dequeue();
     bool isEmpty();
-    void bfs(int);
+    void BFS(int);
     void print_graph();
     void buildQueue(int data[][10], int size)
     {
@@ -105,7 +105,7 @@ bool linkedQueue::isEmpty()
         return false;
 }
 
-void linkedQueue::bfs(int v = 0)
+void linkedQueue::BFS(int v = 0)
 {
     int j;
     visited[v] = true;
@@ -114,7 +114,7 @@ void linkedQueue::bfs(int v = 0)
     while (!isEmpty())
     {
         v = dequeue();
-        cout << v << " -> ";
+        cout << v << " ";
 
         for (j = 0; j < size; j++)
         {
@@ -129,12 +129,19 @@ void linkedQueue::bfs(int v = 0)
 
 void linkedQueue::print_graph()
 {
+    cout << "    ";
     for (int i = 0; i < size; i++)
     {
-        cout << "graph[" << i << "] -> ";
+        cout << "v" << i << " ";
+    }
+    cout << endl;
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << "v" << i << "  ";
         for (int j = 0; j < size; j++)
         {
-            cout << graph[i][j] << " ";
+            cout << graph[i][j] << "  ";
         }
         cout << endl;
     }
@@ -153,31 +160,30 @@ int main()
 
     linkedQueue Q1;
     Q1.buildQueue(graph1, 8);
-    cout << " <<    Graph data 1    >>" << endl
+    cout << " ***    Graph data 1    ***" << endl
          << endl;
     Q1.print_graph();
     cout << endl;
-    cout << "    Breadth first search result:" << endl;
-    Q1.bfs(0);
+    cout << "Breadth first search result: ";
+    Q1.BFS(0);
     cout << endl
          << endl
          << endl;
 
     int graph2[10][10] = {
-        {0, 1, 1, 0, 0, 0, 0},
-        {1, 0, 0, 1, 1, 0, 0},
-        {1, 0, 0, 0, 0, 0, 1},
-        {0, 1, 0, 0, 0, 1, 0},
-        {0, 1, 0, 0, 0, 1, 0},
-        {0, 0, 0, 1, 1, 0, 1},
-        {0, 0, 1, 0, 0, 1, 0}};
+        {0, 1, 1, 0, 0, 0},
+        {1, 0, 1, 1, 0, 0},
+        {1, 1, 0, 0, 0, 1},
+        {0, 1, 0, 0, 1, 1},
+        {0, 0, 0, 1, 0, 1},
+        {0, 0, 1, 1, 1, 0}};
 
     linkedQueue Q2;
-    Q2.buildQueue(graph2, 7);
-    cout << " <<    Graph data 2    >>" << endl
+    Q2.buildQueue(graph2, 6);
+    cout << " ***    Graph data 2    ***" << endl
          << endl;
     Q2.print_graph();
     cout << endl;
-    cout << "    Breadth first search result:" << endl;
-    Q2.bfs(0);
+    cout << "Breadth first search result: ";
+    Q2.BFS(0);
 }
